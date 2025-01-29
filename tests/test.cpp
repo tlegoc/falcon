a8c472d245b0d9aaa82f02d8659e822633b9ff0b
Valère Plantevin <valereplantevin@proton.me>
2025-01-29 14:32:01 -0500

    Implement full tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,12 +1,46 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include "falcon.h"
+
 unsigned int Factorial( unsigned int number ) {
     return number <= 1 ? number : Factorial(number-1)*number;
 }
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+TEST_CASE( "Can Listen", "[falcon]" ) {
+    auto receiver = Falcon::Listen("127.0.0.1", 5555);
+    REQUIRE(receiver != nullptr);
+}
+
+TEST_CASE( "Can Connect", "[falcon]" ) {
+    auto sender = Falcon::Connect("127.0.0.1", 5556);
+    REQUIRE(sender != nullptr);
+}
+
+TEST_CASE( "Can Send To", "[falcon]" ) {
+    auto sender = Falcon::Connect("127.0.0.1", 5556);
+    auto receiver = Falcon::Listen("127.0.0.1", 5555);
+    std::string message = "Hello World!";
+    std::span data(message.data(), message.size());
+    int bytes_sent = sender->SendTo("127.0.0.1", 5555, data);
+    REQUIRE(bytes_sent == message.size());
+}
+
+TEST_CASE("Can Receive From", "[falcon]") {
+    auto sender = Falcon::Connect("127.0.0.1", 5556);
+    auto receiver = Falcon::Listen("127.0.0.1", 5555);
+    std::string message = "Hello World!";
+    std::span data(message.data(), message.size());
+    int bytes_sent = sender->SendTo("127.0.0.1", 5555, data);
+    REQUIRE(bytes_sent == message.size());
+    std::string from_ip;
+    from_ip.resize(255);
+    std::array<char, 65535> buffer;
+    int byte_received = receiver->ReceiveFrom(from_ip, buffer);
+
+    REQUIRE(byte_received == message.size());
+    REQUIRE(std::equal(buffer.begin(),
+        buffer.begin() + byte_received,
+        message.begin(),
+        message.end()));
+    REQUIRE(from_ip == "127.0.0.1:5556");
 }
